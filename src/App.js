b8cf1ff6c3beaf1c54dd69d018e1821ee8fd7ec7
DaniAkash <eaglearrow.arrow@gmail.com>
2016-12-06 09:08:31 +0530

    Tasks Styles

--- src/App.js
+++ src/App.js
@@ -7,22 +7,58 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Hi! Your Todo List!!</h2>
+          <h2>Hi! Welcome to Todo List App!!</h2>
         </div>
         <div className="App-body">
           <div className="user-name-area">
             <input
               className="user-name-input"
               type="text"
-              placeholder="&#xf21d; Please enter your name!"
+              placeholder="&#xf21d; Please enter your name to continue..."
             />
             <button
               type="submit"
               className="Btn user-name-submit">&#xf0c7;</button>
           </div>
-          <p className="tasks">Task 1</p>
-          <p className="tasks">Task 2</p>
-          <p className="tasks">Task 3</p>
+          <div className="tasks-area">
+            <div className="tasks-header">All | Pending | Completed</div>
+            <div className="tasks">
+              <input
+                type="checkbox"
+                className="tasks-checkbox"
+              />
+              <p>Task 1 Task 1 Task 1 Task 1 Task 1 Task 1 Task 1 Task 1 Task 1 Task 1 Task 1 Task 1 Task 1 Task 1 Task 1 Task 1 Task 1 Task 1 Task 1 Task 1 Task 1 Task 1 Task 1 Task 1 </p>
+              <select className="tasks-priority">
+                <option value="high">High</option>
+                <option value="med">Medium</option>
+                <option value="low">Low</option>
+              </select>
+            </div>
+            <div className="tasks">
+              <input
+                type="checkbox"
+                className="tasks-checkbox"
+              />
+              <p>Task 2</p>
+              <select className="tasks-priority">
+                <option value="high">High</option>
+                <option value="med">Medium</option>
+                <option value="low">Low</option>
+              </select>
+            </div>
+            <div className="tasks">
+              <input
+                type="checkbox"
+                className="tasks-checkbox"
+              />
+              <p>Task 3</p>
+              <select className="tasks-priority">
+                <option value="high">High</option>
+                <option value="med">Medium</option>
+                <option value="low">Low</option>
+              </select>
+            </div>
+          </div>
         </div>
       </div>
     );
