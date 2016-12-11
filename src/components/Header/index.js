c06a9e298678e9ecf0906fd52bf838f1a176c2ef
DaniAkash <eaglearrow.arrow@gmail.com>
2016-12-11 23:10:07 +0530

    hide logout in login page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,14 +3,18 @@ import React, {PropTypes} from 'react';
 const Header = ({userName, logout}) =>  {
   return (
     <div className="App-header">
-      <i></i>
+      {(userName !== "") &&
+        <i></i>
+      }
       {(userName === "") &&
         <h2>Hi! Welcome to Todo List App!!</h2>
       }
       {(userName !== "") &&
         <h2>Hi {userName}! Here's the list of your tasks!!</h2>
       }
-      <i className="fa fa-power-off logout-button" onClick={logout}></i>
+      {(userName !== "") &&
+        <i className="fa fa-power-off logout-button" onClick={logout}></i>
+      }
     </div>
   );
 };
